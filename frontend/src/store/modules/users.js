79448220dff8e63a7e5ecb70980acac423250ace
diegochiquero <chiquerodiego@gmail.com>
2020-04-22 18:57:29 +0200

    Feat: Use constants mutation type :gear:

--- frontend/src/store/modules/users.js
+++ frontend/src/store/modules/users.js
@@ -15,29 +15,35 @@ const getters = {
 const actions = {
 	async getAllUsers({ commit }) {
 		const response = await ApiService.fetchUsers()
-		commit('setUsers', response.data.user)
+		commit('SET_USERS', response.data.user)
 	},
 	async addUser({ commit }, user) {
 		const response = await ApiService.sumUpUser(user)
-		commit('newUser', response.data.user)
+		commit('NEW_USER', response.data.user)
 	},
 	async deleteUser({ commit }, id) {
 		await ApiService.removeUser(id)
-		commit('eliminateUser', id)
+		commit('ELIMINATE_USER', id)
 	},
 	async updateUser({ commit }, updUser) {
 		const response = await ApiService.actualize(updUser)
-		commit('upToDateUser', response.data.user)
+		commit('UPDATE_USERS', response.data.user)
 	}
 }
 
 // mutations
 const mutations = {
-	setUsers: (state, users) => (state.users = users),
-	newUser: (state, user) => state.users.unshift(user),
-	eliminateUser: (state, id) =>
-		(state.users = state.users.filter(user => user._id !== id)),
-	upToDateUser: (state, updUser) => {
+	SET_USERS(state, users) {
+		state.users = users
+	},
+	NEW_USER(state, user) {
+		state.users.unshift(user)
+	},
+	ELIMINATE_USER(state, id) {
+		state.users = state.users.filter(user => user._id !== id)
+	},
+
+	UPDATE_USERS(state, updUser) {
 		const index = state.users.findIndex(user => user._id === updUser._id)
 		if (index !== -1) {
 			state.users.splice(index, 1, updUser)
