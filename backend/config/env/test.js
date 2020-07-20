e8312fefb0e0a5f054948a93569935dcf849db51
diegochiquero <chiquerodiego@gmail.com>
2020-07-20 17:20:33 +0200

    Test: User model testing :test_tube:

---
+++ backend/config/env/test.js
@@ -0,0 +1,6 @@
+/*module.exports = {
+    db: 'mongodb://localhost/mevn_test'
+}*/
+
+//This stack backend is using "mongodb-memory-server" for testing 
+//https://github.com/nodkz/mongodb-memory-server
